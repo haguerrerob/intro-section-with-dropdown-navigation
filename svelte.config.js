53cc0d31c21f80dcb252b072e060883fab7fa937
haguerrerob <haguerrerob@gmail.com>
2022-05-01 11:22:12 -0300

    create the /docs folder

--- svelte.config.js
+++ svelte.config.js
@@ -1,20 +1,34 @@
-import preprocess from 'svelte-preprocess';
+// import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
+const dev = 'production' === 'development';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	// 	preprocess: preprocess(),
+
+	// 	kit: {
+	// 		// hydrate the <div id="svelte"> element in src/app.html
+	// 		// target: '#svelte',
+	// 		adapter: adapter({
+	// 			pages: 'build',
+	// 			assets: 'build',
+	// 			fallback: null
+	// 		})
+	// 	}
 
 	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		// target: '#svelte',
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: null
-		})
+			pages: 'docs',
+			assets: 'docs'
+		}),
+		paths: {
+			// change below to your repo name
+			base: dev ? '' : 'https://github.com/haguerrerob/intro-section-with-dropdown-navigation.git'
+		},
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
 	}
 };
 
