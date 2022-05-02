0dd4a765b5349545446678e4597863cbb235f149
haguerrerob <haguerrerob@gmail.com>
2022-05-02 09:33:54 -0300

    Modify the runtime

--- svelte.config.js
+++ svelte.config.js
@@ -37,25 +37,44 @@ const dev = process.env.NODE_ENV === 'development';
 
 // export default config;
 
-import adapter from 'svelte-adapter-github';
+// import adapter from 'svelte-adapter-github';
 
-export default {
+// export default {
+// 	kit: {
+// 		adapter: adapter({
+// 			// default options are shown
+// 			pages: 'docs',
+// 			assets: 'docs',
+// 			domain: null,
+// 			jekyll: false,
+// 			fallback: null,
+// 			precompress: false
+// 		}),
+// 		paths: {
+// 			base: dev ? '' : '/intro-section-with-dropdown-navigation.git'
+// 		},
+// 		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+// 		// For example, instead of '_app', use 'app_', 'internal', etc.
+// 		appDir: 'internal'
+// 	}
+// };
+// };
+
+import adapter from '@sveltejs/adapter-static';
+
+const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown
 			pages: 'docs',
-			assets: 'docs',
-			domain: null,
-			jekyll: false,
-			fallback: null,
-			precompress: false
+			assets: 'docs'
 		}),
 		paths: {
+			// change below to your repo name
 			base: dev ? '' : '/intro-section-with-dropdown-navigation.git'
 		},
-		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
-		// For example, instead of '_app', use 'app_', 'internal', etc.
-		appDir: 'internal'
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
 	}
 };
-// };
+
+export default config;
