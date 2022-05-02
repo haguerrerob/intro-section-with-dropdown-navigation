b4dd3f32542977d7a6eef23794d3535357ffc83e
haguerrerob <haguerrerob@gmail.com>
2022-05-02 11:54:47 -0300

    prerender

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,11 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
 // const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 
 const config = {
+	preprocess: preprocess(),
 	kit: {
 		adapter: adapter({
 			pages: 'docs',
