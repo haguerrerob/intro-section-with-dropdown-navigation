5b796c4cbac323e2f09c5215edc241f925521eee
haguerrerob <haguerrerob@gmail.com>
2022-05-02 06:46:46 -0300

    paths

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@
 import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 
 // const config = {
@@ -27,7 +28,13 @@ const config = {
 			pages: 'docs',
 			assets: 'docs',
 			fallback: null
-		})
+		}),
+		paths: {
+			base: dev ? '' : '/intro-section-with-dropdown-navigation'
+		},
+		// If you are not using a .nojekyll file, change your appDir to something not starting with an underscore.
+		// For example, instead of '_app', use 'app_', 'internal', etc.
+		appDir: 'internal'
 	}
 };
 
