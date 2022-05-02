063e7d74660c8f0fccffbfc3bf9fb174f689f50d
haguerrerob <haguerrerob@gmail.com>
2022-05-02 09:50:05 -0300

    commit on master

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,9 @@
 // import adapter from '@sveltejs/adapter-auto';
 
-import preprocess from 'svelte-preprocess';
+// import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 
 // const config = {
@@ -18,16 +19,21 @@ import adapter from '@sveltejs/adapter-static';
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: preprocess(),
+	// preprocess: preprocess(),
 
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
 		// target: '#svelte',
 		adapter: adapter({
 			pages: 'docs',
-			assets: 'docs',
-			fallback: null
-		})
+			assets: 'docs'
+		}),
+		paths: {
+			// change below to your repo name
+			base: dev ? '' : '/intro-section-with-dropdown-navigation.git'
+		},
+		// hydrate the <div id="svelte"> element in src/app.html
+		target: '#svelte'
 	}
 };
 
