1b1845d888be96a93566e4e3322011c331b59494
haguerrerob <haguerrerob@gmail.com>
2022-05-02 22:08:37 -0300

    data-hydrate

--- svelte.config.js
+++ svelte.config.js
@@ -4,19 +4,45 @@ import adapter from '@sveltejs/adapter-static';
 // const dev = process.env.NODE_ENV === 'development';
 /** @type {import('@sveltejs/kit').Config} */
 
+// const config = {
+// 	preprocess: preprocess(),
+// 	kit: {
+// 		adapter: adapter({
+// 			pages: 'docs',
+// 			assets: 'docs',
+// 			fallback: 'index.html'
+// 		})
+// paths: {
+// 	// 	// change below to your repo name
+// 	base: dev ? '' : ''
+// }
+// prerender: { default: true }
+// 	}
+// };
+
+// export default config;
+
+// import adapter from 'svelte-adapter-github';
+
+// const dev = process.env.NODE_ENV === 'development';
+
 const config = {
 	preprocess: preprocess(),
+
 	kit: {
 		adapter: adapter({
+			// default options are shown
 			pages: 'docs',
 			assets: 'docs',
+			// domain: null,
+			// jekyll: false,
 			fallback: 'index.html'
-		})
-		// paths: {
-		// 	// 	// change below to your repo name
-		// 	base: dev ? '' : ''
-		// }
-		// prerender: { default: true }
+			// precompress: false
+		}),
+		paths: {
+			// base: dev ? '' : '/intro-section-with-dropdown-navigation'
+		},
+		appDir: '_app'
 	}
 };
 
