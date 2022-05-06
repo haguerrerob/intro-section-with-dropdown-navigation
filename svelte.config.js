f855aac583c39d574ddbd604c04673f5ac480ad5
haguerrerob <haguerrerob@gmail.com>
2022-05-06 10:15:40 -0300

    Modify to deploy in Vercel

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,8 @@
-import preprocess from 'svelte-preprocess';
-import adapter from '@sveltejs/adapter-static';
+// import preprocess from 'svelte-preprocess';
+// import adapter from '@sveltejs/adapter-static';
 
 // const dev = process.env.NODE_ENV === 'development';
-/** @type {import('@sveltejs/kit').Config} */
+// /** @type {import('@sveltejs/kit').Config} */
 
 // const config = {
 // 	preprocess: preprocess(),
@@ -24,25 +24,36 @@ import adapter from '@sveltejs/adapter-static';
 
 // import adapter from 'svelte-adapter-github';
 
-const dev = process.env.NODE_ENV === 'development';
+// const dev = process.env.NODE_ENV === 'development';
 
+// const config = {
+// 	preprocess: preprocess(),
+
+// 	kit: {
+// 		adapter: adapter({
+// 			// default options are shown
+// 			pages: 'docs',
+// 			assets: 'docs',
+// 			// domain: null,
+// 			// jekyll: false,
+// 			fallback: 'index.html'
+// 			// precompress: false
+// 		}),
+// 		paths: {
+// 			base: dev ? '' : '/intro-section-with-dropdown-navigation/docs'
+// 		},
+// 		appDir: '_app'
+// 	}
+// };
+
+// export default config;
+
+import adapter from '@sveltejs/adapter-auto';
+
+/** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
-
 	kit: {
-		adapter: adapter({
-			// default options are shown
-			pages: 'docs',
-			assets: 'docs',
-			// domain: null,
-			// jekyll: false,
-			fallback: 'index.html'
-			// precompress: false
-		}),
-		paths: {
-			base: dev ? '' : '/intro-section-with-dropdown-navigation/docs'
-		},
-		appDir: '_app'
+		adapter: adapter()
 	}
 };
 
